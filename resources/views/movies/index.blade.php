f6d37aaa477c91d01ae0d026c05d805a583ace74
Horvath Mark <horvath.mark1@students.jedlik.eu>
2024-12-14 21:57:58 +0100

    card style

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,19 +1,28 @@
 <x-layout>
-    <h1>Movies</h1>
+    <h1 class="text-center mb-4">Movies</h1>
     <div class="row ">
         @foreach ($movies as $movie)
-            <div class="col-md-3 mb-3">
-                <div class="card h-100">
-                    <div class="card-header">
-                        <h5 class="card-title">{{ $movie->title }}</h5>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">{{ $movie->director }}</li>
-                        <li class="list-group-item">{{ $movie->release_year }}</li>
-                        <li class="list-group-item">{{ $movie->rating }} </li>
-                    </ul>
+        <div class="col-md-3 mb-3">
+            <div class="card h-100 shadow-lg">
+                <div class="card-header bg-primary text-white text-center" style="border-top-left-radius: 15px; border-top-right-radius: 15px;">
+                    <h4 class="card-title">{{ $movie->title }}</h4>
                 </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        <b>Director:</b> 
+                        <span class="badge bg-info text-black fs-6">{{ $movie->director }}</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        <b>Release year:</b> 
+                        <span class="badge bg-success text-white fs-6">{{ $movie->release_year }}</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        <b>IMDB Rating:</b> 
+                        <span class="badge bg-warning text-dark fs-7">{{ $movie->rating }}</span>
+                    </li>
+                </ul>
             </div>
+        </div>        
         @endforeach
     </div>
 </x-layout>
